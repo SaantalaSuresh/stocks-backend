fb840d347ce4a08658cef996fb46c5fe8df30a54
Suresh <saantalasuresh@gmail.com>
2024-09-05 21:43:58 +0530

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const AuthRouter = require("./routes/authRoutes")
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use("/api/auth", AuthRouter);
 mongoose.connect(process.env.MONGODBURL)
 .then(()=>{
     console.log("MONGODB connected Successfully");
-    app.listen(PORT,()=>{
+    app.listen(4000,()=>{
         console.log("Server is running on port 4000");
     })
 })
